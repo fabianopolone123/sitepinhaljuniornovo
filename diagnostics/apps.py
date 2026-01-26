a243afb6531235ddf541a97e5faff0c811e42b8b
fabianopolone <fabianopolone@gmail.com>
2026-01-26 18:33:42 -0300

    Feito modificações no mercadopago

--- diagnostics/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class DiagnosticsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'diagnostics'
-    verbose_name = 'Diagnostics'
--- diagnostics/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('health/', views.health_check, name='diagnostics-health'),
-    path('logs/', views.recent_logs, name='diagnostics-logs'),
-]
